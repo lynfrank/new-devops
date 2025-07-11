73678ed1d0a034e213ffa53b2c6861dabc2b3f2c
stanilpaul <stanilpaul@gmail.com>
2025-07-11 23:51:36 +0200

    new update

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -106,7 +106,7 @@ function AddItemForm({ onNewItem }) {
                         disabled={!newItem.length}
                         className={submitting ? 'disabled' : ''}
                     >
-                        {submitting ? 'Adding...' : 'HAHA'}
+                        {submitting ? 'Adding...' : 'WHAT BRO'}
                     </Button>
                 </InputGroup.Append>
             </InputGroup>
